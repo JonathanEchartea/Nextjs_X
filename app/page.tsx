a8dff6db2a71030af6fe8a56209c36e6363fddd0
Jonathan_Montoya <jonathan_montoya@epamneoris.com>
2026-04-06 15:37:13 -0600

    Configura Prisma con postgresql y Prisma Client

--- app/page.tsx
+++ app/page.tsx
@@ -1,43 +1,19 @@
-'use client';
+import prisma from "@/lib/prisma";
 
-import Link from "next/link";
-import Image from "next/image";
-import React from "react";
-
-export default function Home() {
-  const [ contador, setContador ] = React.useState<number>(0);
-
-  const width = 200;
-  const height = 200;
+export default async function Home() {
+  const users = await prisma.user.findMany();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black">
-        <div>Main</div>
-        <Link href="/configuraciones">Admin</Link>
-        <Image src="next.svg" alt="Vercel Logo" width={width} height={height} />
-        <h1>Contador: {contador}</h1>
-        <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-      </main>
-    { Home2() }
-    </div>
-  );
-}
-
-
-export function Home2() {
-  const [ contador, setContador ] = React.useState<number>(0);
-
-  const width = 200;
-  const height = 200;
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-black">
-        <div>Main</div>
-        <Link href="/configuraciones">Admin</Link>
-        <Image src="https://tech.sparkfabrik.com/images/content/nextjs/nextjs-logo.jpg" alt="Vercel Logo" width={width} height={height} />
-        <h1>Contador: {contador}</h1>
-        <button onClick={() => setContador(contador + 1)}>Incrementar</button>
-      </main>
+    <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
+      <h1 className="text-4xl font-bold mb-8 font-[family-name:var(--font-geist-sans)] text-[#333333]">
+        Superblog
+      </h1>
+      <ol className="list-decimal list-inside font-[family-name:var(--font-geist-sans)]">
+        {users.map((user) => (
+          <li key={user.id} className="mb-2">
+            {user.name}
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
